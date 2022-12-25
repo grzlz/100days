dd37659353147b9f683152053570b42fbb277acc
grzlz <grzlz@outlook.es>
2022-12-25 13:56:44 -0500

    remove print and use only input

--- 1-band.py
+++ 1-band.py
@@ -1,10 +1,6 @@
-print("What's the name of the city you grew up in?")
+city = input("What's the name of the city you grew up in? ")
 
-city = input()
-
-print("What's your pet's name")
-
-pet = input()
+pet = input(("What's your pet's name? "))
 
 band_name = f"Your band name could be {city} {pet}"
 
