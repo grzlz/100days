79a9c9356f684d3cdf0292befeba67c1f729d452
grzlz <grzlz@outlook.es>
2022-12-25 13:38:23 -0500

    start 100 days of python challenge by completing first task

---
+++ 1-band.py
@@ -0,0 +1,12 @@
+print("What's the name of the city you grew up in?")
+
+city = input()
+
+print("What's your pet's name")
+
+pet = input()
+
+band_name = f"Your band name could be {city} {pet}"
+
+
+print(band_name)
