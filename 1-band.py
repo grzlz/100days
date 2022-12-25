e20b05921605815466ad705b523acf0f8507c42e
grzlz <grzlz@outlook.es>
2022-12-25 14:08:51 -0500

    add space with backslash n

--- 1-band.py
+++ 1-band.py
@@ -1,6 +1,6 @@
-city = input("What's the name of the city you grew up in? ")
+city = input("What's the name of the city you grew up in?\n")
 
-pet = input(("What's your pet's name? "))
+pet = input(("What's your pet's name?\n"))
 
 band_name = f"Your band name could be {city} {pet}"
 
