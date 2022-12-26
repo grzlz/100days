027ba6c510cfd0a82c597f38b1dd1490d69da719
grzlz <grzlz@outlook.es>
2022-12-25 19:21:08 -0500

    add elif on nested if

--- 3-rollercoaster.py
+++ 3-rollercoaster.py
@@ -1,13 +1,15 @@
-# Check if height > 120 and age > 18. If > 18, pay 12, else 7
+# Check if height > 120 and age > 18. If > 18, pay 12, 12-17, pay 7, < 12 5
 
-height = int(input("Introduce height. "))
+height = int(input("Introduce height: "))
 
 
 if height > 120:
-    age = int(input("Introduce age. "))
-    if age > 18:
+    age = int(input("Introduce age: "))
+    if age >= 18:
         print("Pay 12.")
-    else:
-        print("Pay $7")
+    elif age < 18 and age >= 12:
+        print("Pay $7.")
+    else: 
+        print("Pay $5.")
 else:
     print("No ride for you :/")
