899e629f812ff2e4f55cdf81939ac76fa82cd77b
grzlz <grzlz@outlook.es>
2022-12-29 15:01:45 -0500

    complete treasure island game

--- 3-rollercoaster.py
+++ 3-rollercoaster.py
@@ -9,3 +9,17 @@ direction = input("You land in a desert island. You have to choose which directi
 if direction == "l":
     exit("You fell into a cliff and died. Game over.")
 
+print("You arrived to a river. You can see an old tree crossing it and cave by the other end of it.")
+river = input("Do you swim or try to cross the tree? (s/c)\n")
+
+if  river == "s":
+    exit("The current is too strong, you end up fallin from a waterfall to your death. Game over.")
+
+print("In the cave you see three doors. The first one is blue, the second red and the third is yellow. Choose one (b/r/y).")
+
+door = input("Choose a door. (b/r/y)\n")
+
+if door == "b":
+    print("You found the treasure!")
+else:
+    exit("Game over.")
