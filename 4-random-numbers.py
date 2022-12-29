39bc9983b4fbe566976ec630a61bbe86197098e0
grzlz <grzlz@outlook.es>
2022-12-29 16:01:25 -0500

    fix index length

--- 4-random-numbers.py
+++ 4-random-numbers.py
@@ -5,8 +5,8 @@ names = input("Introduce the names of the involved bankers separated by a comma.
 
 names_list = names.split(", ")
 
-list_length = len(names_list) - 1
+list_length = len(names_list)
 
-choose_index = random.randint(0, list_length)
+choose_index = random.randint(0, list_length - 1)
 
 print(f"{names_list[choose_index]} will pay the bill.a,")
