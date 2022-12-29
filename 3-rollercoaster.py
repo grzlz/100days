a778b62d4bfda18132efba15dc706b86d6e82ad2
grzlz <grzlz@outlook.es>
2022-12-29 14:53:16 -0500

    start treasure island quest

--- 3-rollercoaster.py
+++ 3-rollercoaster.py
@@ -1,22 +1,11 @@
-# Check if height > 120 and age > 18. If > 18, pay 12, 12-17, pay 7, < 12 5
+from sys import exit
 
-height = int(input("Introduce height: "))
+# Treasure island hunt
 
+print("Welcome to Python Treasure Island! Your mission is to find the reasure.")
 
-if height > 120:
-    
-    age = int(input("Introduce age: "))
-    photo = input("Do you want a photo? (y/n)\n")
-    if age < 12:
-        cost = 5
-    elif age <= 18:
-        cost = 7
-    else: 
-        cost = 12
-    
-    if photo == "y":
-        cost += 2
+direction = input("You land in a desert island. You have to choose which direction to go. Left or right? (l/r).\n")
 
-    print(f"Please pay ${cost}.")
-else:
-    print("No ride for you :/")
+if direction == "l":
+    exit("You fell into a cliff and died. Game over.")
+
