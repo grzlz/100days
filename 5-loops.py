237c424925af780273a7f8ead90d986eb0791498
grzlz <grzlz@outlook.es>
2022-12-30 17:24:54 -0500

    complete with lame randomization logic at the end

--- 5-loops.py
+++ 5-loops.py
@@ -1,6 +1,6 @@
 # Create a password generator 
 
-from random import randint, choice
+from random import shuffle, choice
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -12,43 +12,21 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
 
-letter_acum = 0
-number_acum = 0
-symbol_acum = 0
+pwd_list = []
 
+for letter in range(1, nr_letters + 1):
+    pwd_list += choice(letters)
 
+for number in range(1, nr_numbers + 1):
+    pwd_list += choice(numbers)
 
-input_pwd_length = nr_letters + nr_numbers + nr_symbols
-pwd = ''
+for symbol in range(1, nr_symbols + 1):
+    pwd_list += choice(symbols)
 
-for pwd_length in range(1, input_pwd_length + 1):
-    random_number_for_character_type = randint(1, 3)
+shuffle(pwd_list)
 
-    if random_number_for_character_type == 1:
-        if letter_acum < nr_letters:
-            random_number_for_letter = randint(0, len(letters) - 1)
-            pwd += letters[random_number_for_letter]
-            letter_acum += 1
-
-        else:
-            random_number_for_character_type = randint(2, 3)
-
-    if random_number_for_character_type == 2:
-        if number_acum < nr_numbers:
-            random_number_for_number = randint(0, len(numbers) - 1)
-            pwd += numbers[random_number_for_number]
-            number_acum += 1
-
-        else:
-            random_number_for_character_type = choice([1, 3])
-
-    if random_number_for_character_type == 3:
-        if symbol_acum < nr_symbols:
-            random_number_for_symbol = randint(0, len(symbols) - 1)
-            pwd += symbols[random_number_for_symbol]
-            symbol_acum += 1
-
-        else:
-            random_number_for_character_type = choice([1, 2])
+pwd = ""
+for i in pwd_list:
+    pwd += i
 
 print(pwd)
