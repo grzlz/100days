9c96e1e9e4f1ed4ab2d6f68c5e7651ee935d522f
grzlz <grzlz@outlook.es>
2022-12-30 14:32:16 -0500

    create range exercise

--- 5-loops.py
+++ 5-loops.py
@@ -1,15 +1,4 @@
 # Calculate the highest score from a list
 
-input_string = input("Enter student's height separated by a space:\n")
-
-input_list = input_string.split()
-
-input_list = [int(i) for i in input_list]
-
-max_value = 0
-
-for i in input_list:
-    if i >= max_value:
-        max_value = i
-
-print(max_value)
+for i in range(1, 10):
+    print(i)
