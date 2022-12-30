5abebbbc70e8b33974edfa236309f612785883af
grzlz <grzlz@outlook.es>
2022-12-30 16:25:45 -0500

    try my best

--- 5-loops.py
+++ 5-loops.py
@@ -1,6 +1,6 @@
 # Create a password generator 
 
-from random import randint
+from random import randint, choice
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -24,25 +24,31 @@ pwd = ''
 for pwd_length in range(1, input_pwd_length + 1):
     random_number_for_character_type = randint(1, 3)
 
-    if random_number_for_character_type == 1 and letter_acum < nr_letters:
-        # Select a random letter for next character in string 
+    if random_number_for_character_type == 1:
+        if letter_acum < nr_letters:
+            random_number_for_letter = randint(0, len(letters) - 1)
+            pwd += letters[random_number_for_letter]
+            letter_acum += 1
 
-        random_number_for_letter = randint(0, len(letters) - 1)
-        pwd += letters[random_number_for_letter]
-        letter_acum += 1
+        else:
+            random_number_for_character_type = randint(2, 3)
 
-    if random_number_for_character_type == 2 and number_acum < nr_numbers:
+    if random_number_for_character_type == 2:
+        if number_acum < nr_numbers:
+            random_number_for_number = randint(0, len(numbers) - 1)
+            pwd += numbers[random_number_for_number]
+            number_acum += 1
 
-        random_number_for_number = randint(0, len(numbers) - 1)
-        pwd += numbers[random_number_for_number]
-        number_acum += 1
+        else:
+            random_number_for_character_type = choice([1, 3])
 
-    if random_number_for_character_type == 3 and symbol_acum < nr_symbols:
+    if random_number_for_character_type == 3:
+        if symbol_acum < nr_symbols:
+            random_number_for_symbol = randint(0, len(symbols) - 1)
+            pwd += symbols[random_number_for_symbol]
+            symbol_acum += 1
 
-        random_number_for_symbol = randint(0, len(symbols) - 1)
-        pwd += symbols[random_number_for_symbol]
-        symbol_acum += 1
-
-
+        else:
+            random_number_for_character_type = choice([1, 2])
 
 print(pwd)
