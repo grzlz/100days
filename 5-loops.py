b95b41b2832b357542017bf29d5e050aef9c7b78
grzlz <grzlz@outlook.es>
2022-12-30 14:46:54 -0500

    add fizzbuz

--- 5-loops.py
+++ 5-loops.py
@@ -1,7 +1,15 @@
-# Calculate the sum of all even number from 1 to 100 including 100
+# Fizz buz. If number divisible by 3, print Fizz, if divisible by 5 print Buzz. If divisible by 3 and 5, print fizzbuz
 
-acumulator = 0
-for i in range(2, 101, 2):
-    acumulator += i
-
-print(acumulator)
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizzbuz")
+    
+    elif i % 3 == 0:
+        print("Fizz")
+    
+    elif 1 % 5 == 0:
+        print("Buzz")
+    
+    else:
+        print(i)
+    
