0d5f592311d51e8e2f5c7c540d411aa273a46022
grzlz <grzlz@outlook.es>
2022-12-30 15:18:12 -0500

    try for first time

--- 5-loops.py
+++ 5-loops.py
@@ -1,6 +1,7 @@
 # Create a password generator 
 
-import random
+from random import randint
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -9,10 +10,32 @@ print("Welcome to the PyPassword Generator!")
 # nr_letters= int(input("How many letters would you like in your password?\n")) 
 # nr_symbols = int(input(f"How many symbols would you like?\n"))
 # nr_numbers = int(input(f"How many numbers would you like?\n"))
- 
+
+input_pwd_length = 4
 pwd = ''
 
-pwd += letters[0]
-pwd += letters[4]
+for pwd_length in range(1, input_pwd_length + 1):
+    random_number_for_character_type = randint(1, 3)
+
+    if random_number_for_character_type == 1:
+        # Select a random letter for next character in string 
+
+        random_number_for_letter = randint(1, len(letters))
+
+        pwd += letters[random_number_for_letter]
+
+    if random_number_for_character_type == 2:
+
+        random_number_for_number = randint(1, len(numbers))
+
+        pwd += numbers[random_number_for_number]
+
+    if random_number_for_character_type == 3:
+
+        random_number_for_symbol = randint(1, len(symbols))
+
+        pwd += symbols[random_number_for_symbol]
+
+
 
 print(pwd)
