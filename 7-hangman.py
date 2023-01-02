a16629c438fe9408626890d6834f18eca1696cc3
grzlz <grzlz@outlook.es>
2023-01-02 15:05:37 -0500

    loop while game is not over

--- 7-hangman.py
+++ 7-hangman.py
@@ -4,8 +4,7 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = choice(word_list)
 
-guessed_letter = input("Guess a letter.\n")
-guessed_letter = guessed_letter.lower()
+
 
 
 
@@ -14,17 +13,18 @@ display = []
 for i in chosen_word:
     display.append("_")
 
+# Use a while loop to let the user guess again until there are no more "_" to display.
 while "_" in display:
-    for i in range(len(display)):
-        print(display     )
-        display[i] = 'a'
+    guessed_letter = input("Guess a letter.\n")
+    guessed_letter = guessed_letter.lower()
 
+    for i in range(len(chosen_word)):
 
-for i in range(len(chosen_word)):
-
-    if guessed_letter == chosen_word[i]:
+        if guessed_letter == chosen_word[i]:
+            display[i] = guessed_letter
+            print(display)
         
-        display[i] = guessed_letter
-    
 
 
+if "_" not in display:
+    print("You won!")   
