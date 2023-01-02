bfbb1b66f8fb8f37d85446d84f8674aca6baf859
grzlz <grzlz@outlook.es>
2023-01-02 17:15:14 -0500

    finish the game as you can

--- 7-hangman.py
+++ 7-hangman.py
@@ -1,14 +1,73 @@
 from random import choice
 
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = choice(word_list)
 
 
+# Create a variable called 'lives' to keep track of the number of lifes left. Set lives equal to 6
 
+lives = 6
 
-
-
+# If guess is not a letter in chosen word, then reduce lives by 1. If lifes goes down to 0 then the game should stop and print 'You lose'.
 display = []
 for i in chosen_word:
     display.append("_")
@@ -18,13 +77,22 @@ while "_" in display:
     guessed_letter = input("Guess a letter.\n")
     guessed_letter = guessed_letter.lower()
 
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        
+
     for i in range(len(chosen_word)):
 
         if guessed_letter == chosen_word[i]:
             display[i] = guessed_letter
-            print(display)
-        
 
+    print(stages[lives])
+    print(display)
 
-if "_" not in display:
-    print("You won!")   
+    if lives == 0:
+        print("You lose!")
+        break
+
+    if "_" not in display:
+        print("You won!")   
+        break
