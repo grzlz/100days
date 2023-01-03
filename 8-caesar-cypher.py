5ecf2660e2d13253540cb414f046a6d675d80325
grzlz <grzlz@outlook.es>
2023-01-03 13:13:51 -0500

    create file and write greet function

---
+++ 8-caesar-cypher.py
@@ -0,0 +1,9 @@
+# Create a new function called greet()
+# Write 3 print statements, call the function and run the code
+
+def greet():
+    print("Hola")
+    print("Funciona")
+    print("Necesito aprender shortcuts en el teclado")
+
+greet()
