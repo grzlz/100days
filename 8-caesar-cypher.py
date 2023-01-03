f10a547637f633ac8dd670e1e656616d4485e3a2
grzlz <grzlz@outlook.es>
2023-01-03 13:19:13 -0500

    add parameter name to function

--- 8-caesar-cypher.py
+++ 8-caesar-cypher.py
@@ -1,9 +1,11 @@
 # Create a new function called greet()
 # Write 3 print statements, call the function and run the code
 
-def greet():
-    print("Hola")
+# Add name argument to function
+
+def greet(name):
+    print(f"Hola {name}")
     print("Funciona")
     print("Necesito aprender shortcuts en el teclado")
 
-greet()
+greet("hola")
