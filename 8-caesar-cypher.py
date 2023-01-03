94093b9c7901ffd8d4c8f71a482d4edc1c9ac3aa
grzlz <grzlz@outlook.es>
2023-01-03 14:50:11 -0500

    remove return and add found_prime variable and logic

--- 8-caesar-cypher.py
+++ 8-caesar-cypher.py
@@ -3,10 +3,17 @@
 input_number = int(input("Introduce a number: "))
 
 def prime_number(input_number):
+    found_prime = True
     for number in range(2, input_number):
         if input_number % number == 0:
-            return(print("Not a prime number."))
-        else: 
-            return(print("It is a primer number!"))
-    
+            found_prime = False
+
+
+    if found_prime:
+        print("It's a prime number!")
+
+    else:
+        print("It's not a prime number.")
+
+
 prime_number(input_number)
