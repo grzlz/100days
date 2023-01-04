48a98dfe031365a627e3b235da73403692f3f2c3
grzlz <grzlz@outlook.es>
2023-01-04 14:57:19 -0500

    complete bidder

--- 9-dictionaries.py
+++ 9-dictionaries.py
@@ -1,29 +1,47 @@
-# Travel log
+# Auction bidder
+from os import system
 
-travel_log = [
-    {
-    "country_visited": "France", 
-    "cities_visited": ["Paris", "Lille", "Dijon"],
-    "total_visits": 12
-    },
-    {
-    "country_visited": "Germany",
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-    "total_visits": 16
-    }
-]
+# Create an empty list of bids
 
-# Write a function that will add a new country to the list
+bids = []
 
-def add_new_country(country_name, cities_name, visits):
-    new_country = {
-        "country_visited": country_name,
-        "cities_visited": cities_name,
-        "total_visits": visits
+# Create a function that stores name and bid
+def bidder(name, bid):
+    new_bidder = {
+        "name": name,
+        "bid": bid
     }
 
-    travel_log.append(new_country)
+    bids.append(new_bidder)
 
-add_new_country("Russia", ["Moscow", "SP"], 3)
+# Create a function that returns the name and bid of the highest bid
+def get_highest_bid(bids_list):
+    
+    highest_bidder = bids_list[0]["name"]
+    highest_bid = bids_list[0]["bid"]
 
-print(travel_log)
+    for bid in bids_list:
+        if bid["bid"] > highest_bid:
+            highest_bid = bid["bid"]
+            highest_bidder = bid["name"]
+
+    print(f"The highest bidder is {highest_bidder} for ${highest_bid}.")
+
+
+# Create a condition and loop while True
+new_bid = True
+
+while new_bid:
+    new_name = input("What is your name? ")
+    new_bid = int(input("What is your bid? $"))
+    bidder(new_name, new_bid)
+
+    if input("Are there more bidders? ") == "No":
+        new_bid = False
+    
+    system("clear")
+
+
+
+get_highest_bid(bids)
+
