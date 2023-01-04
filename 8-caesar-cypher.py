4113599856c1010bb34fa25665b592f4e212404d
grzlz <grzlz@outlook.es>
2023-01-03 22:08:50 -0500

    add decrypt funtionality and if statement logic

--- 8-caesar-cypher.py
+++ 8-caesar-cypher.py
@@ -4,9 +4,11 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 # Create a function called 'encrypt' that takes the text and shift as inputs
 # Inside the 'encrypt' function, shift each letter of the text forwards in the alphabet by the shift amount and print the encripted text
+# Check if user wanted to encrypt or decrypt the message. 
+
 
 def encrypt(text, shift):
-    
+
     encrypted_word = ""
 
     for i in text:
@@ -14,7 +16,35 @@ def encrypt(text, shift):
         letter_index = alphabet.index(i)
         encrypted_word += alphabet[letter_index + shift]
 
-    print(f"The encoded word is {encrypted_word}")
+    print(f"The encrypted word is {encrypted_word}.")
 
-encrypt("zulu", 1)
+
+def decrypt(text, shift):
+
+    decrypted_word = ""
+
+    for i in text:
+        letter_index = alphabet.index(i)
+        decrypted_word += alphabet[letter_index - shift]
+
+    print(f"The decrypted word is {decrypted_word}.")
+
+
+task = input("Do you want to encode or decode?\n")
+
+if task == "encode":
+    text_to_encode = input("Text to encode: ")
+
+    encrypt(text_to_encode, 1)
+
+elif task == "decode":
+    text_to_decode = input("Text to decode: ")
+    
+    decrypt(text_to_decode, 1)
+
+
+
+
+
+
  
