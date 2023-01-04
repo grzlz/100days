de6ee8215846bc0203d39f58bddee959ca384f5f
grzlz <grzlz@outlook.es>
2023-01-04 14:04:38 -0500

    write program to edit dict

--- 9-dictionaries.py
+++ 9-dictionaries.py
@@ -1,13 +1,29 @@
 # Travel log
 
-travel_log = {
-    "France": {
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12
+travel_log = [
+    {
+    "country_visited": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
     },
-
-    "Germany": {
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 16
+    {
+    "country_visited": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 16
     }
-}
+]
+
+# Write a function that will add a new country to the list
+
+def add_new_country(country_name, cities_name, visits):
+    new_country = {
+        "country_visited": country_name,
+        "cities_visited": cities_name,
+        "total_visits": visits
+    }
+
+    travel_log.append(new_country)
+
+add_new_country("Russia", ["Moscow", "SP"], 3)
+
+print(travel_log)
