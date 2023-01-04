9aa0ddd0510f360bd3a01a7b9ec179276a1ba08c
grzlz <grzlz@outlook.es>
2023-01-04 13:54:40 -0500

    create nested dictionary

--- 9-dictionaries.py
+++ 9-dictionaries.py
@@ -1,33 +1,13 @@
-# Write a program than converts their scores to grades. 
-# Score 91-100, Outstanding
-# 81-90 "Exceeds expectations"
-# 71-80 "Acceptable
-#  70 < Fail
+# Travel log
 
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 16
+    }
 }
-
-student_grades = {}
-
-for key in student_scores:
-    grade = ""
-    if student_scores[key] < 70:
-        grade = "Fail"
-
-    elif student_scores[key] <= 80:
-        grade = "Acceptable"
-
-    elif student_scores[key] <= 90:
-        grade = "EX"
-
-    else:
-        grade = "Outstanding"
-    
-    student_grades[key] = grade
-
-print(student_grades)
