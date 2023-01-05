fe92c3c94bc669938fb8e6349e21830c427e417c
grzlz <grzlz@outlook.es>
2023-01-04 22:17:51 -0500

    get days in month and consider leap years

--- 10-functions.py
+++ 10-functions.py
@@ -1,20 +1,30 @@
-# Functions with outputs
+# Create a function called days_in_month() which will take a year and a month as inputs
+# and will use this informantion to return the number of days in the month
 
-def format_name(f_name, l_name):
-    f_name = f_name.lower()
-    l_name = l_name.lower()
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else: 
+                return False
+                print("No leap year :(")
+        else: 
+            return True
+    else:
+        return False
 
-    f_name = [i for i in f_name]
-    l_name = [i for i in l_name]
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
 
-    f_name[0] = f_name[0].upper()
-    l_name[0] = l_name[0].upper()
+    if is_leap(year):
+        month_days[1] += 1
 
-    f_name = "".join(f_name)
-    l_name = "".join(l_name)
+    return month_days[month - 1]
 
-    name = f"{f_name} {l_name}"
-    return name
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
 
-print(format_name("JUANITO", "JONES"))
+days = days_in_month(year, month)
 
+print(days)
