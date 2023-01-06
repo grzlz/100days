1be3ac788a77ec5029a3de42bee9e4779de74aee
grzlz <grzlz@outlook.es>
2023-01-05 21:31:12 -0500

    make a recursive function called calculator

--- 10-functions.py
+++ 10-functions.py
@@ -19,28 +19,30 @@ calculator_dict = {
     "/": divide
 }
 
-num1 = int(input("What's the first number? "))
-for i in calculator_dict:
-    print(i)
+def calculator():
+    num1 = int(input("What's the first number? "))
+    for i in calculator_dict:
+        print(i)
 
-keep_going = True
-while keep_going:
+    keep_going = True
+    while keep_going:
 
-    operation_symbol = input("Pick an operation: ")
+        operation_symbol = input("Pick an operation: ")
 
-    num2 = int(input("What's the next number?: "))
+        num2 = int(input("What's the next number?: "))
 
-    function = calculator_dict[operation_symbol]
-    answer = function(num1, num2)
+        function = calculator_dict[operation_symbol]
+        answer = function(num1, num2)
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
 
+        else:
+            keep_going = False
+            calculator()
 
-    if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit: ") == "y":
-        num1 = answer
-
-    else:
-        keep_going = False
+calculator()
 
     
