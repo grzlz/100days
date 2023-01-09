5e677e350e4a1f6dde8c36265341266f848d18ab
grzlz <grzlz@outlook.es>
2023-01-09 12:51:44 -0500

    complete number guessing game

--- 12-scope.py
+++ 12-scope.py
@@ -1,11 +1,51 @@
-# Scope
+# Mke guessing game
+from random import randint 
+from os import system
+from sys import exit
 
-enemies = 1
+logo = """
+ __  _ _  _ __ __ __ ___ ___    __  __  __ __ ___  
+|  \| | || |  V  |  \ __| _ \  / _]/  \|  V  | __| 
+| | ' | \/ | \_/ | -< _|| v / | [/\ /\ | \_/ | _|  
+|_|\__|\__/|_| |_|__/___|_|_\  \__/_||_|_| |_|___| 
+"""
 
-def increase_enemies():
-    global enemies
-    enemies += 3
-    print(f"enemies inside function: {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+print(f"{logo}\nWelcome to the Number Guessing Game.\nI'm thinking of a random number between 1 and 100.")
+
+valid_option = False
+while valid_option is not True:
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+    lives = 0
+    if difficulty == "easy":
+        lives = 10
+        valid_option = True
+    elif difficulty == "hard":
+        lives = 5
+        valid_option = True
+    else:
+        system("clear")
+        print("Please select a valid option.")
+
+
+secret_number = randint(1, 100)
+
+right_guess = False
+print(secret_number)
+print(f"You have {lives} lives to guess the secret number.")
+while lives > 0 and right_guess is not True:
+    guessed_number = int(input("Take a guess: "))
+    
+    if guessed_number > secret_number:
+        lives -= 1
+        print(f"Too high! Remaining lives: {lives}.")
+
+    elif guessed_number < secret_number:
+        lives -= 1
+        print(f"Too low! Remaining lives: {lives}.")
+
+    else:
+        print(f"You guessed correctly! The number is {secret_number}!")
+        right_guess = True
+
