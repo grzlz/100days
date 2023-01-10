aa13303414502ab25b20d058be8405291b707923
grzlz <grzlz@outlook.es>
2023-01-10 15:58:15 -0500

    remove redundant while loop

--- 16-oop/oop-coffee-machine-start/main.py
+++ 16-oop/oop-coffee-machine-start/main.py
@@ -13,21 +13,14 @@ valid_drink = False
 
 while is_on:
     user_input = input(f"What yould you like? Options are {menu.get_items()}:\n")
-
     if user_input == "off":
         is_on = False
-
     elif user_input == "report":
         machine.report()
         money.report()
-
     else:
-        while valid_drink is False:
-            beverage = menu.find_drink(user_input)
-            if beverage:
-                valid_drink = True
-                if machine.is_resource_sufficient(beverage):
-                    if money.make_payment(beverage.cost):
-                        machine.make_coffee(beverage)
-            else:
-                break
+        beverage = menu.find_drink(user_input)
+        if beverage:
+            if machine.is_resource_sufficient(beverage):
+                if money.make_payment(beverage.cost):
+                    machine.make_coffee(beverage)
