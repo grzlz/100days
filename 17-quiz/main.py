385f980ac59631f71d37df2ca4bfa375f6065b1e
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-11 13:43:20 -0500

    create constructor and print new user created

--- 17-quiz/main.py
+++ 17-quiz/main.py
@@ -1,9 +1,14 @@
 class User:
-    pass
+
+    def __init__(self):
+        print("New user being created...")
 
 user_1 = User()
 
 user_1.id = "001"
 user_1.username = "memo"
-
 print(user_1.username)
+
+user_2 = User()
+user_2.name = "many"
+print(user_2.name)
