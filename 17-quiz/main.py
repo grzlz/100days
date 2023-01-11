d901c74fe99a417ecb78e1dd79ceedc97f963dce
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-11 13:46:26 -0500

    add two attributes to constructor and create two objects

--- 17-quiz/main.py
+++ 17-quiz/main.py
@@ -1,14 +1,12 @@
 class User:
 
-    def __init__(self):
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
         print("New user being created...")
 
-user_1 = User()
-
-user_1.id = "001"
-user_1.username = "memo"
+user_1 = User("001", "memo")
 print(user_1.username)
 
-user_2 = User()
-user_2.name = "many"
+user_2 = User("002", "many")
 print(user_2.name)
