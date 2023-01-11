1017efae8751ed6e910fa40af8c082e45a672326
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-11 14:03:31 -0500

    add follow method and make user1 follow user 2 and print both users followers

--- 17-quiz/main.py
+++ 17-quiz/main.py
@@ -3,10 +3,20 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
-        print("New user being created...")
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 user_1 = User("001", "memo")
 print(user_1.username)
 
 user_2 = User("002", "many") 
 print(user_2.username)
+
+user_1.follow(user_2)
+print(user_1.followers, user_1.following)
+print(user_2.followers, user_2.following)
