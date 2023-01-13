2a23be9a212bbe47376448c452f75b21c4e169a5
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-13 17:18:55 -0500

    create plygons

--- 18-turtle/main.py
+++ 18-turtle/main.py
@@ -4,9 +4,22 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("green")
 
-for i in range(4):
-    timmy.fd(100)
-    timmy.right(90)
+# Draw a pentagon 
+
+figures = [3, 4, 5, 6, 7, 8, 9, 10]
+colors = ["blue", "green", "red", "yellow", "purple", "green", "blue", "red"]
+
+
+for figure in range(len(figures)):
+    timmy.pencolor(colors[figure])
+    for i in range(figures[figure]):
+        timmy.fd(100)
+        timmy.right(360/figures[figure])
+
+    
+
+
+
 
 screen = Screen()
 screen.exitonclick()
