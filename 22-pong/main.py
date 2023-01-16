e6822f53c06529feec9be296668e2f9e20e1a8c6
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-16 14:08:29 -0500

    create ball and try to make it bounce

--- 22-pong/main.py
+++ 22-pong/main.py
@@ -1,5 +1,6 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from paddle import Paddle
+from time import sleep
 
 WIDTH = 1000
 HEIGHT = 700
@@ -19,9 +20,30 @@ screen.onkeypress(key="s", fun=left_paddle.move_down)
 screen.onkeypress(key="Up", fun=right_paddle.move_up)
 screen.onkeypress(key="Down", fun=right_paddle.move_down)
 
+# Create the ball and make it move
+
+ball = Turtle()
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.setheading(0)
+
 is_game_on = True
+iter = 0
 while is_game_on:
     screen.update()
+    sleep(0.1)
+
+    if abs(ball.position() - right_paddle.position()) <= 15 or abs(ball.position() - left_paddle.position())<= 15:
+        current_heading = ball.heading()
+        new_heading = 160 - current_heading
+        ball.setheading(new_heading)
+
+    ball.forward(15)
+
+    print(ball.position(), right_paddle.position(), abs(ball.position() - right_paddle.position()))
+
+    iter += 1
 
 screen.exitonclick()
 
