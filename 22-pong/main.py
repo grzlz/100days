d6f074fe66375395877d23f4d64ebd03bf1eead6
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-16 12:07:15 -0500

    this is me trying

--- 22-pong/main.py
+++ 22-pong/main.py
@@ -1,7 +1,24 @@
-from court import Court
+from turtle import Screen
 from paddle import Paddle
+WIDTH = 1000
+HEIGHT = 700
+
+right_paddle = Paddle("right")
+left_paddle = Paddle("left")
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=WIDTH, height=HEIGHT)
+ 
+
+screen.listen()
+screen.onkey(key="w", fun=left_paddle.move_up)
+screen.onkey(key="s", fun=left_paddle.move_down)
+screen.onkey(key="Up", fun=right_paddle.move_up)
+screen.onkey(key="Down", fun=right_paddle.move_down)
 
 
-court = Court()
-paddle = Paddle()
-print(paddle.paddle_segments)
+screen.exitonclick()
+
+
+
--- 22-pong/paddle.py
+++ 22-pong/paddle.py
@@ -1,13 +1,35 @@
 from turtle import Turtle
 
 PADDLE_LENGTH = 5
+PADDLE_POSITION = 450
 
 class Paddle:
-    def __init__(self):
+    def __init__(self, input_side):
         self.paddle_segments = []
+        self.heading = 0
+        side = 1
+        if input_side == "left":
+            side = -1
+
         for i in range(PADDLE_LENGTH):
             paddle = Turtle()
             paddle.shape("square")
             paddle.color("white")
-            paddle.goto(0, i * -20)
+            paddle.penup()
+            paddle.speed("fastest")
+            paddle.goto(PADDLE_POSITION * side, i * -20)
             self.paddle_segments.append(paddle)
+
+    def move_up(self):
+        for paddle in self.paddle_segments:
+            paddle.setheading(90)
+            paddle.fd(20)
+
+    def move_down(self):
+        for paddle in self.paddle_segments:
+            paddle.setheading(270)
+            paddle.fd(20)
+
+    
+
+
