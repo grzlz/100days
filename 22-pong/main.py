59b30e18d45e2a1e751c7560154799d84d06a9b7
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-16 13:18:06 -0500

    remove screen class and refactor paddle class

--- 22-pong/main.py
+++ 22-pong/main.py
@@ -1,22 +1,27 @@
 from turtle import Screen
 from paddle import Paddle
+
 WIDTH = 1000
 HEIGHT = 700
 
 right_paddle = Paddle("right")
 left_paddle = Paddle("left")
 
-screen = Screen()
+screen = Screen() 
 screen.bgcolor("black")
 screen.setup(width=WIDTH, height=HEIGHT)
+screen.tracer(0)
  
 
 screen.listen()
-screen.onkey(key="w", fun=left_paddle.move_up)
-screen.onkey(key="s", fun=left_paddle.move_down)
-screen.onkey(key="Up", fun=right_paddle.move_up)
-screen.onkey(key="Down", fun=right_paddle.move_down)
+screen.onkeypress(key="w", fun=left_paddle.move_up)
+screen.onkeypress(key="s", fun=left_paddle.move_down)
+screen.onkeypress(key="Up", fun=right_paddle.move_up)
+screen.onkeypress(key="Down", fun=right_paddle.move_down)
 
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
 
--- 22-pong/paddle.py
+++ 22-pong/paddle.py
@@ -3,32 +3,26 @@ from turtle import Turtle
 PADDLE_LENGTH = 5
 PADDLE_POSITION = 450
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, input_side):
-        self.paddle_segments = []
-        self.heading = 0
-        side = 1
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.pu()
+        self.speed("fastest")
+        self.shapesize(stretch_len=5)
+        self.setheading(90)
+        self.direction = 1
         if input_side == "left":
-            side = -1
+            self.direction = -1
+        self.goto(self.direction * 450, 0)
 
-        for i in range(PADDLE_LENGTH):
-            paddle = Turtle()
-            paddle.shape("square")
-            paddle.color("white")
-            paddle.penup()
-            paddle.speed("fastest")
-            paddle.goto(PADDLE_POSITION * side, i * -20)
-            self.paddle_segments.append(paddle)
 
     def move_up(self):
-        for paddle in self.paddle_segments:
-            paddle.setheading(90)
-            paddle.fd(20)
+        self.forward(20)
 
     def move_down(self):
-        for paddle in self.paddle_segments:
-            paddle.setheading(270)
-            paddle.fd(20)
+        self.forward(-20)
 
     
 
