d4c1e084dbb4c05102912adb930f1c82c89e0b28
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-17 10:00:54 -0500

    create turtle and define win condition

--- 23-turtle-crossing/main.py
+++ 23-turtle-crossing/main.py
@@ -6,7 +6,13 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.listen()
 screen.tracer(0)
+
+player = Player()
+
+screen.onkeypress(key="w", fun=player.move)
+screen.onkeypress(key="s", fun=player.move_bd)
 
 game_is_on = True
 while game_is_on:
--- 23-turtle-crossing/player.py
+++ 23-turtle-crossing/player.py
@@ -1,7 +1,32 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
+FINISH_LINE_Y = 290
 
 
-class Player:
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.win_condition = False
+
+    def move(self):
+        if self.ycor() < FINISH_LINE_Y:
+            self.forward(MOVE_DISTANCE)
+
+        else:
+            self.win_condition = True
+            print(self.ycor())
+
+    def move_bd(self):
+        self.forward(-MOVE_DISTANCE)
+        
+
+
+
     pass
