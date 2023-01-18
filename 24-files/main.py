21cf1bdcd59fe1ef16f9831600fb1dc132f57491
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-18 12:46:00 -0500

    move template letter out of for loop, cause we are not writing on it, so its enough opening once

--- 24-files/main.py
+++ 24-files/main.py
@@ -6,12 +6,10 @@ with open("./Input/Names/invited_names.txt") as file:
     guests = file.readlines()
     guests = [guest.strip() for guest in guests]
 
-print(guests)
+with open("./Input/Letters/starting_letter.txt", mode="r") as template:
+    template_letter = template.read()
 
 for guest in guests:
-    with open("./Input/Letters/starting_letter.txt", mode="r") as template:
-        template_letter = template.read()
-
     with open(f"./Output/ReadyToSend/letter_to_{guest}.txt", mode="w") as file:
         modified_letter = template_letter.replace("[name]", guest)
         file.write(modified_letter)
