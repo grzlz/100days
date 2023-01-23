67f2da540a78667fb31144869dd07c83251c7c90
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-23 09:50:21 -0500

    read with csv library

--- 25-csv/main.py
+++ 25-csv/main.py
@@ -1,5 +1,16 @@
 #Open the weather_data.csv User .readlines() to create a list named data that contains the values from the .csv file
-with open("./weather_data.csv") as file:
- data = file.readlines()
+# with open("./weather_data.csv") as file:
+# data = file.readlines()
 
-print(data)
+# print(data)
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
