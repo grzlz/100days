59f9732ffb855a2c2b7d1ec17109cc57f045edad
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-23 10:03:24 -0500

    use pandas library to read file

--- 25-csv/main.py
+++ 25-csv/main.py
@@ -4,13 +4,37 @@
 
 # print(data)
 
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
+# with open("weather_data.csv") as data_file:
+#    data = csv.reader(data_file)
+#    temperatures = []
+#    for row in data:
+#        if row[1] != "temp":
+#            temperatures.append(int(row[1]))
+#    print(temperatures)
 
-    print(temperatures)
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+# print(data["temp"])
+data_dict = data.to_dict()
+print(data_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
