8292cbd60f2c1bc7b25269af17115a3c83e30ee8
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-24 10:28:32 -0500

    do first exercise, split the list

--- 26-list-comprehension/main.py
+++ 26-list-comprehension/main.py
@@ -1,10 +1,5 @@
-from random import randint
-# dictionary comprehension
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-students_dict = {name: randint(1, 100) for name in names}
-print(students_dict)
+# Exercise 1
+sentence = "What is the Airspeed Veolcity of an Unladen Swallow?"
 
-# Create passed students dict
-
-passed_students = {name:grade for (name, grade) in students_dict.items() if grade > 50}
-print(passed_students)
+strike1 = {word: len(word) for word in sentence.split()}
+print(strike1)
