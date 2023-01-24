f9a0ec2a34af90d53f35e9ad8d2b34ad4f1597f4
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-24 10:23:55 -0500

    if statement on dict comprehension

--- 26-list-comprehension/main.py
+++ 26-list-comprehension/main.py
@@ -1,5 +1,10 @@
+from random import randint
 # dictionary comprehension
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-
-students_dict = {name: 10 for name in names}
+students_dict = {name: randint(1, 100) for name in names}
 print(students_dict)
+
+# Create passed students dict
+
+passed_students = {name:grade for (name, grade) in students_dict.items() if grade > 50}
+print(passed_students)
