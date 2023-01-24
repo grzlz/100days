6e74478200695f713bb376407b342082a77ae6c8
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-24 11:07:39 -0500

    final commit

--- 26-list-comprehension/main.py
+++ 26-list-comprehension/main.py
@@ -5,6 +5,5 @@ d = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in d.iterrows()}
 
 word = input("What word do you wish to NATOfy? ")
-
 answer = [nato_dict.get(i.upper()) for i in word]
 print(answer)
