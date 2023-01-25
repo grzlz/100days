a2c13adc21a664a80e4d68af8cc6a3c7cf1ea13d
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-25 14:45:19 -0500

    create miles to km converter

--- 27-gui/main.py
+++ 27-gui/main.py
@@ -1,27 +1,37 @@
 from tkinter import * 
 
+def button_click():
+    km = int(input.get()) * 1.6
+    km_output.config(text=km)
+
 window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 window.config(padx=20, pady=20)
 
-# Label
-my_label = Label(text="I am a label!", font=("Arial", 18, "bold"))
+# Inital label
+my_label = Label(text="Miles to km converter", font=("Arial", 18, "bold"))
 my_label.grid(column=0, row=0)
-my_label.config(padx=50, pady=50)
-# Button
-button = Button(text="Click me!")
-button.grid(column=1, row=1)
-
-# New button
-new_button = Button(text="I'm the new button!")
-new_button.grid(column=2, row=0)
-
 # Entry
 input = Entry(width=10)
 input.grid(column=3, row=2)
+# Miles label
+miles_label = Label(text="miles", font=("Arial", 10))
+miles_label.grid(column=4, row=2)
+# Km space
+km_output = Label(text="0", font=("Arial", 10))
+km_output.grid(column=3, row=3)
+# Km label
+km_label = Label(text="km", font=("Arial", 10))
+km_label.grid(column=4, row=3)
+# Button
+button = Button(text="Convert", command=button_click)
+button.grid(column=3, row=4)
 
+# Create miles to km converter
 
+# Create initial label
+# Create  
 
 
 
