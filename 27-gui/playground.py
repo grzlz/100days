3cfc5bbcf18514a7ac24f5500fa4467a78875a8e
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-25 12:51:12 -0500

    add a calculate function that uses kwargs

--- 27-gui/playground.py
+++ 27-gui/playground.py
@@ -6,4 +6,12 @@ def add(*args):
         sum += n
     return sum
 
-print(add(2, 3, 1))
+# print(add(2, 3, 1))
+
+def calculate(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+calculate(argumento1=1, brgumento2=2)
