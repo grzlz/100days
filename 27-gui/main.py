629b47d027a46cb5edad5b4ddc5c8073f0840146
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-25 13:53:24 -0500

    use grid to display objects and add padding

--- 27-gui/main.py
+++ 27-gui/main.py
@@ -3,29 +3,23 @@ from tkinter import *
 window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
-my_label = Label(text="I am a label!", font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+my_label = Label(text="I am a label!", font=("Arial", 18, "bold"))
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+# Button
+button = Button(text="Click me!")
+button.grid(column=1, row=1)
 
-
-def button_click():
-    my_label.config(text=input.get())
-
-
-# Create button
-button = Button(text="Click me!", command=button_click)
-button.pack()
+# New button
+new_button = Button(text="I'm the new button!")
+new_button.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
-
-
-
-
-
-
+input.grid(column=3, row=2)
 
 
 
