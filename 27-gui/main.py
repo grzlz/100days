35e89505a85b803c241efbb631d78893afdd5ff9
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-25 13:16:01 -0500

    create button and add functionality to gui

--- 27-gui/main.py
+++ 27-gui/main.py
@@ -1,18 +1,21 @@
-import tkinter
+from tkinter import * 
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a label!", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a label!", font=("Arial", 24, "bold"))
 my_label.pack(expand=True)
 
 
+def button_click():
+    my_label.config(text="Button clicked!")
 
 
-
-
+# Create button
+button = Button(text="Click me!", command=button_click)
+button.pack()
 
 
 
