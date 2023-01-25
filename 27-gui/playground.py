d9f753d0d3a6f71dc8e95abf9e0079606c95d89b
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-25 12:56:13 -0500

    create class car and show benefits of using .get when working with dictionaries

--- 27-gui/playground.py
+++ 27-gui/playground.py
@@ -15,3 +15,14 @@ def calculate(**kwargs):
         print(value)
 
 calculate(argumento1=1, brgumento2=2)
+
+# Benefits of using .get on dictonaries and optional keywords
+
+class Car:
+    def __init__(self, **kwargs):
+        self.maker = kwargs.get("maker")
+        self.model = kwargs.get("model")
+
+my_car = Car(maker="Nissan")
+print(my_car.maker)
+# This prints None, if I had used kwargs["model"] if would return an error.
