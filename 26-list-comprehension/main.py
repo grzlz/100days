e6305c6fcbacc3d30bc7d70827b83767b94ffd11
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-29 10:53:28 -0500

    loop until provided word exist in dictionary keys

--- 26-list-comprehension/main.py
+++ 26-list-comprehension/main.py
@@ -4,6 +4,13 @@ import pandas as pd
 d = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in d.iterrows()}
 
-word = input("What word do you wish to NATOfy? ")
-answer = [nato_dict.get(i.upper()) for i in word]
-print(answer)
+
+answer = []
+while not answer:
+    word = input("What word do you wish to NATOfy? ").upper()
+    try:
+        answer = [nato_dict[i] for i in word]
+    except KeyError as message:
+        print(f"Sorry, only letters. Key {message} does not exist.")
+    else:
+        print(answer)
