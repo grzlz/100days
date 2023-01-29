1f7cdbddcfc91dac26066686951db20d7f563bc4
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-29 10:11:59 -0500

    use all keywords for error flow

--- 30-errors/main.py
+++ 30-errors/main.py
@@ -1,4 +1,15 @@
 try:
     file = open("file.txt", "r")
-except:
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
     file = open("file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
