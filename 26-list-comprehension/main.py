8448f056b7577e68006349f11823848d8653d9fa
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-29 10:54:39 -0500

    create a function with the same functionality and make it recursive on error

--- 26-list-comprehension/main.py
+++ 26-list-comprehension/main.py
@@ -5,12 +5,14 @@ d = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in d.iterrows()}
 
 
-answer = []
-while not answer:
+def generate_word():
     word = input("What word do you wish to NATOfy? ").upper()
     try:
         answer = [nato_dict[i] for i in word]
     except KeyError as message:
         print(f"Sorry, only letters. Key {message} does not exist.")
+        generate_word()
     else:
         print(answer)
+
+generate_word()
