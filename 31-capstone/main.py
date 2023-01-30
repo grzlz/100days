c75c7f67735d09f826faf3f162cb0a87dd91533c
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-30 13:06:17 -0500

    add color

--- 31-capstone/main.py
+++ 31-capstone/main.py
@@ -4,9 +4,9 @@ BACKGROUND_COLOR = "#B1DDC6"
 # UI Setup
 window = Tk()
 window.title("Learn french")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=525)
+canvas = Canvas(width=800, height=525, background=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
 rigt_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
 front_canvas = PhotoImage(file="images/card_front.png")
@@ -14,7 +14,7 @@ back_canvas = PhotoImage(file="images/card_back.png")
 
 canvas.create_image(400, 260, image=back_canvas)
 canvas.create_image(400, 260, image=front_canvas)
-title_text = canvas.create_text(400, 170 , text="Title", fill="black", font=("Courier", 25, "bold"))
+title_text = canvas.create_text(400, 170 , text="Title", fill="black", font=("Courier", 20))
 word_text = canvas.create_text(400, 260 , text="Word", fill="black", font=("Courier", 25, "bold"))
 
 
