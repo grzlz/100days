6c326d1cee1a7bc525cb85325b39f73eb58494e0
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-30 15:25:21 -0500

    soy muy bueno

--- 31-capstone/main.py
+++ 31-capstone/main.py
@@ -1,5 +1,31 @@
 from tkinter import *
+from random import choice
+import pandas as pd
+from time import sleep
+
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+# Read file
+
+data = pd.read_csv("data/french_words.csv")
+words = data.to_dict(orient="records")
+
+# Select random word
+
+
+# Display on word_text
+
+def next_word():
+    word = choice(words)
+    fr = word["French"]
+    en = word["English"]
+    canvas.itemconfig(word_text, text=fr)
+    window.after(1000, display_word, en)
+
+def display_word(en):
+    canvas.itemconfig(word_text, text=en)
+    window.after(1000, next_word)
 
 # UI Setup
 window = Tk()
@@ -20,7 +46,7 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 
 # Buttons
-right_button = Button(image=rigt_img)
+right_button = Button(image=rigt_img, command=next_word)
 wrong_button = Button(image=wrong_img)
 
 right_button.grid(column=1, row=2)
