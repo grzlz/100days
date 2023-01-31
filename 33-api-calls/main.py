babe09d9548bb60cd2194f739274b42150235f51
grzlz <guillermo.rdzlpz@gmail.com>
2023-01-31 16:03:51 -0500

    use kanye quote api

--- 33-api-calls/main.py
+++ 33-api-calls/main.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.json().get("iss_position"))
+response = requests.get(url="https://api.kanye.rest/")
+print(response.json().get("quote"))
