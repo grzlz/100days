3b7b5cc1d7fed44c6016bcbaa2ab521ff8848936
grzlz <guillermo.rdzlpz@gmail.com>
2023-02-03 14:29:21 -0500

    exclude current, minutely and daily params

--- 35-apis-keys/main.py
+++ 35-apis-keys/main.py
@@ -1,19 +1,15 @@
 import requests 
 
-api_key = "69f04e4613056b159c2761a9d9e664d2"
-parameters = {
-    "q": "Cancun",
-    "appid": api_key
-    }
-
-r = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
-print(r.json())
+# api_key = "69f04e4613056b159c2761a9d9e664d2"
+api_key = "b239ad6816f4e09d41069588e7ac2d80"
 
 # Get Hourly forecast for 48 hours from https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+# api_key = "a843491b794fffdf8d6796331a8b6cb1"
 
 parameters = {
     "lat": 21.1743,
     "lon": -86.8466,
+    "exclude": "current,minutely,daily",
     "appid": api_key
 }
 
