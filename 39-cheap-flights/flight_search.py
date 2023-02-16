e1055e02b74c6e8b601c2c9c1296305a22e8a9b2
grzlz <guillermo.rdzlpz@gmail.com>
2023-02-15 19:48:37 -0500

    use tequila api to get iata code if empty. define fetch_code function

--- 39-cheap-flights/flight_search.py
+++ 39-cheap-flights/flight_search.py
@@ -1,6 +1,27 @@
+import requests 
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def get_code(self, data):
         for i in data:
             if i["iataCode"] == "":
-                i["iataCode"] = "TESING"
+
+                i["iataCode"] = self.fetch_code(i["city"])
+
+    def fetch_code(self, city):
+
+        url = "https://api.tequila.kiwi.com/locations/query"
+        params = {
+            "term": city,
+            "locale": "en-US",
+            "location_types": "airport",
+            "limit": 1,
+            "active_only": "true"
+        }
+        headers = {
+            "apikey": "MtYDAqQ8v5jzNh6vBSAt5gZbaVuTnDwW"
+        }
+        r = requests.get(url, params = params, headers = headers)
+        iata_code = r.json()["locations"][0]["id"]
+
+        return iata_code
