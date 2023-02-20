3028fe722a3472c8340e47344ca1802b0fc7929f
grzlz <guillermo.rdzlpz@gmail.com>
2023-02-20 13:38:51 -0500

    fix slicing bug on for loop, send tex message

--- 39-cheap-flights/flight_search.py
+++ 39-cheap-flights/flight_search.py
@@ -1,12 +1,12 @@
-import requests 
+import requests
 from twilio.rest import Client
 
 account_sid = "AC81f2671430dc9505aaed34057ce8f306"
-auth_token = "f294e23376f119d371252473411de6e9"
+auth_token = ""
 
 
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
     def get_code(self, data):
         for i in data:
             if i["iataCode"] == "":
@@ -24,11 +24,11 @@ class FlightSearch:
         headers = {
             "apikey": "MtYDAqQ8v5jzNh6vBSAt5gZbaVuTnDwW"
         }
-        r = requests.get(url, params = params, headers = headers)
+        r = requests.get(url, params=params, headers=headers)
         iata_code = r.json()["locations"][0]["id"]
 
         return iata_code
-    
+
     def get_flight_data(self, destination):
         params = {
             "fly_from": "CUN",
@@ -52,18 +52,19 @@ class FlightSearch:
         flight_data = {key: r[key] for key in r if key in ["price", "local_departure"]}
 
         return flight_data
-    
+
     def find_deal(self, data):
         for destination in data:
             threshold = destination["lowestPrice"]
             destination_code = destination["iataCode"]
             flight_data = self.get_flight_data(destination_code)
-            if threshold <= flight_data["price"]:
+            flight_price = flight_data["price"]
+            if flight_price <= threshold:
                 message_data = {
-                    "city": data["city"],
+                    "city": destination["city"],
                     "price": flight_data["price"],
                     "departure": flight_data["local_departure"]
-                    }
+                }
                 self.send_notification(message_data)
 
     def send_notification(self, data):
@@ -75,5 +76,4 @@ class FlightSearch:
                             to='+525548017016')
 
 
-            # TO DO 
-
+# TO DO
