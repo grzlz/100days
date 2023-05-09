1ecbd4174f1fd3db4caa94d22571a26f9e02c4bb
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-09 14:37:01 -0500

    restart project

--- 45-bs4/bs4-start/main.py
+++ 45-bs4/bs4-start/main.py
@@ -1,14 +1,6 @@
 from bs4 import BeautifulSoup
 
-with open("website.html") as file: 
-    contents = file.read()
+with open("website.html") as file:
+    content = file.read()
 
-soup = BeautifulSoup(contents, "html.parser")
-print(soup.ul)
-
-all_anchor_tags = soup.find_all("a")
-
-for tag in all_anchor_tags:
-    print(tag.getText())
-
-print(soup.find(class_ = "heading"))
+    print(content)
