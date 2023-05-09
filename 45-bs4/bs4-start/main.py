8ebb7305a24c580e2badca73cf3daa1bfab67d93
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-09 14:53:16 -0500

    get text from all p tags using list comprehension

--- 45-bs4/bs4-start/main.py
+++ 45-bs4/bs4-start/main.py
@@ -4,5 +4,12 @@ with open("website.html") as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "html.parser")
-#print(soup.title.string)
-print(soup.prettify())
+
+# Get all p tags
+all_p_tags = soup.find_all(name = "p")
+print(all_p_tags)
+# Returns a list containing all the p tags
+
+# Get text on all p tags
+all_p_text = [p.text for p in all_p_tags]
+print(all_p_text)
