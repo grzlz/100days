037581a673ef5b7b19bc56e6f2f20ced5c4d13a1
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-12 20:44:43 -0500

    use strip to remove blank space in name

--- 46-playlist/main.py
+++ 46-playlist/main.py
@@ -5,4 +5,4 @@ site = requests.get("https://www.billboard.com/charts/hot-100/2022-06-05/").text
 soup = BeautifulSoup(site, "html.parser")
 songs = soup.select("li ul li h3")
 
-print(songs[0])
+print(songs[0].text.strip())
