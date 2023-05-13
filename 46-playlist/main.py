24a2acec63e68edf232d470a80dfd054512ff8d0
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-12 20:44:05 -0500

    use tag to select all names

--- 46-playlist/main.py
+++ 46-playlist/main.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 
 site = requests.get("https://www.billboard.com/charts/hot-100/2022-06-05/").text
 soup = BeautifulSoup(site, "html.parser")
-print(soup)
+songs = soup.select("li ul li h3")
+
+print(songs[0])
