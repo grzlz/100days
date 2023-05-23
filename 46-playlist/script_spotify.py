f5858798af0a5a94a438cb2f572593cb99ed2c2b
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-22 22:00:07 -0500

    use second example

--- 46-playlist/script_spotify.py
+++ 46-playlist/script_spotify.py
@@ -1,14 +1,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+results = spotify.artist_top_tracks(lz_uri)
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
