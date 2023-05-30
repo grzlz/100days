3fac54d888caf0306bb4ef0b7d912a70b3964932
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-29 20:47:25 -0500

    finish first version of the project

--- 46-playlist/main.py
+++ 46-playlist/main.py
@@ -1,7 +1,9 @@
 from scraper import get_song_list
-from script_spotify import get_uris
+from script_spotify import get_uris, create_playlist
 
-user_date = input("Introduce una fecha para iniciar tu viaje musical.")
+user_date = input("Introduce una fecha para iniciar tu viaje musical (formato YYYY-MM-DD): ")
 songs = get_song_list(user_date)
+uris = get_uris(songs)
 
-print(get_uris(songs))
+if __name__ == "__main__":
+    create_playlist(uris)
--- 46-playlist/script_spotify.py
+++ 46-playlist/script_spotify.py
@@ -1,5 +1,6 @@
 import os
 import spotipy
+from json import dumps
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -11,10 +12,8 @@ client_secret = os.getenv('CLIENT_SECRET')
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri="http://example.com",
-                                               scope="user-library-read"))
-results = sp.current_user_saved_tracks()
+                                               scope="playlist-modify-private"))
 user_id = sp.current_user()["id"]
-
 def get_uris(songs):
     uris = []
     for song in songs:
@@ -22,15 +21,10 @@ def get_uris(songs):
 
     return uris
 
+
+def create_playlist(uris):
+    playlist_id = sp.user_playlist_create(user_id, "Spotipy Billboard", public=False).get("id")
+    sp.playlist_add_items(playlist_id=playlist_id, items= uris)
+
+
 # print(sp.search("Time pinkfloyd", limit=1).get("tracks").get("items")[0].get("uri"))
-
-""" 
-scope = "playlist-modify-private"
-auth_manager = SpotifyOAuth(scope=scope)
-sp = spotipy.Spotify(auth_manager=auth_manager)
-
-
-sp.user_playlist_create(user_id, "Spotipy", public=False)
-
-# Next, find 10 songs URIS from scraped list
-# Search spotify for the song from step 1 """
