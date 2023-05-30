7c3fa12af50b29e597722a197889468f5f3aec94
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-29 20:21:11 -0500

    test get uris function - it works!

--- 46-playlist/main.py
+++ 46-playlist/main.py
@@ -1,4 +1,7 @@
 from scraper import get_song_list
+from script_spotify import get_uris
 
 user_date = input("Introduce una fecha para iniciar tu viaje musical.")
 songs = get_song_list(user_date)
+
+print(get_uris(songs))
