7e6365a3e5ab748930335060e51f0a7e036f3b45
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-29 19:14:52 -0500

    get last 20 saved songs

--- 46-playlist/script_spotify.py
+++ 46-playlist/script_spotify.py
@@ -9,6 +9,16 @@ client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri="http://example.com",
+                                               scope="user-library-read"))
+
+results = sp.current_user_saved_tracks()
+
+print(results)
+
+""" 
 scope = "playlist-modify-private"
 auth_manager = SpotifyOAuth(scope=scope)
 sp = spotipy.Spotify(auth_manager=auth_manager)
@@ -18,4 +28,4 @@ user_id = sp.current_user()["id"]
 sp.user_playlist_create(user_id, "Spotipy", public=False)
 
 # Next, find 10 songs URIS from scraped list
-# Search spotify for the song from step 1
+# Search spotify for the song from step 1 """
