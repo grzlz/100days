c6f4033e0829375c3c0a766e034ac7c372f2a064
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-29 20:10:50 -0500

    fix identation

--- 46-playlist/scraper.py
+++ 46-playlist/scraper.py
@@ -1,11 +1,9 @@
 import requests 
 from bs4 import BeautifulSoup
 
-
-
- def get_song_list(date):
+def get_song_list(date):
     site = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/").text
     soup = BeautifulSoup(site, "html.parser")
     songs = soup.select("li ul li h3")
     names = [tag.text.strip() for tag in songs]
-    print(names)
+    return names
